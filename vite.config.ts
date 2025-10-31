663298d9a483acf94f0121dd18e0162a4cb7cd03
Wilson Sneyder Aguilar Velarde <i2310597@continental.edu.pe>
2025-10-31 14:09:44 -0500

    Primero intento de despliegue 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Base para Vercel
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,5 +16,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist', // Carpeta de salida
   },
 })
