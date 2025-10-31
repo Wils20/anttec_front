3ddb8326a7fed5f5bb379aba4c335a35b646f166
Wilson Sneyder Aguilar Velarde <i2310597@continental.edu.pe>
2025-10-31 14:32:15 -0500

    Primero intento de despliegue 11

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
   },
-  base: './', // Muy importante para que Vercel sirva correctamente los assets
+  base: './', // importante para Vercel
 })
