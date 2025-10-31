796b26b2e1fb9b18c46de1e1adf1e98f5b669c7a
Wilson Sneyder Aguilar Velarde <i2310597@continental.edu.pe>
2025-10-31 14:17:10 -0500

    Primero intento de despliegue 7

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // Esto es clave para que los imports de JS/CSS sean relativos
+  base: './', // 🔑 clave para que los scripts se carguen correctamente
   plugins: [
     vue(),
     vueDevTools(),
